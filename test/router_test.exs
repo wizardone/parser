4e1e672186c74643ab7ae6e80d180bc40f3c7d94
Stefan Slaveykov <wizard.oneandonly@gmail.com>
2017-01-11 13:39:11 +0200

    add test for multiple rules matched

--- test/router_test.exs
+++ test/router_test.exs
@@ -15,12 +15,22 @@ defmodule RouterTest do
     assert Router.parse(uri) == {:ok, "No rules added"}
   end
 
-  test "parse successful" do
+  test "parse single rule successful" do
     function = fn(request) -> String.match?(request, ~r/query/) end
     Router.Rule.add(:query, function)
     uri = "http://test.com/?query=omg"
 
     assert Router.parse(uri) == [ok: :query]
+  end
+
+  test "parse multiple rules successful" do
+    function_1 = fn(request) -> String.match?(request, ~r/query/) end
+    function_2 = fn(request) -> String.match?(request, ~r/search/) end
+    Router.Rule.add(:query, function_1)
+    Router.Rule.add(:search, function_2)
+    uri = "http://test.com/?query=omg&search=true"
+
+    assert Router.parse(uri) == [ok: :query, ok: :search]
   end
 
   test "parse unsuccessful" do
